1eb23aaf7b445f30cd4f9c4058ce337b7fcc4a97
fina <syarafina251@gmail.com>
2024-06-13 08:15:59 +0000

    SELESAI MEMBUAT COMMENT

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
 
   get 'home/index'
   get 'read/:article_id', to: 'home#read', as: "read"
+  post 'save-comment/:article_id', to: 'home#save_comment', as: "save_comment"
+  
   resources :articles
   resources :users
+
   root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
